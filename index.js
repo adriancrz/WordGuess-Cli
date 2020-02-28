cdf8bdca67643c213927bd7ef747738d91c8093e
adriancrz93@gmail.com <adriancrz93@gmail.com>
2020-02-28 14:30:48 -0600

    added code for word.js as well as fixing issue with word list on index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Word = require('./Word.js');
+const Word = require('./word.js');
 const inquirer = require('inquirer');
 
 var target;
@@ -6,117 +6,97 @@ var targetWord;
 var guesses;
 var guessesLeft;
 
-const wordList = ["Addison Road", "Anacostia", "Archives",
-    "Arlington Cemetery", "Ballston MU", "Benning Road", "Bethesda",
-    "Braddock Road", "Branch Avenue", "Brookland CUA",
-    "Capitol Heights", "Capitol South", "Cheverly", "Clarendon", "Cleveland Park",
-    "College Park University of Maryland", "Columbia Heights", "Congress Heights",
-    "Court House", "Crystal City", "Deanwood", "Dunn Loring", "Dupont Circle",
-    "East Falls Church", "Eastern Market", "Eisenhower Avenue", "Farragut North",
-    "Farragut West", "Federal Center SW", "Federal Triangle", "Foggy Bottom GWU",
-    "Forest Glen", "Fort Totten", "Franconia Springfield", "Friendship Heights",
-    "Gallery Place", "Georgia Avenue Petworth", "Glenmont", "Greenbelt",
-    "Greensboro", "Grosvenor Strathmore", "Huntington", "Judiciary Square",
-    "King Street Old Town", "LEnfant Plaza", "Landover", "Largo Town Center",
-    "McLean", "McPherson Square", "Medical Center", "Metro Center",
-    "Minnesota Avenue", "Morgan Boulevard", "Mount Vernon Square",
-    "Navy Yard Ballpark", "Naylor Road", "New Carrollton", "NoMa Gallaudet U",
-    "Pentagon", "Pentagon City", "Potomac Avenue", "Prince Georges Plaza",
-    "Rhode Island Avenue Brentwood", "Rockville",
-    "Ronald Reagan Washington National Airport", "Rosslyn", "Shady Grove",
-    "Shaw Howard University", "Silver Spring", "Smithsonian", "Southern Avenue",
-    "Spring Hill", "Stadium Armory", "Suitland", "Takoma", "Tenleytown AU",
-    "Twinbrook", "Tysons Corner", "U Street", "Union Station", "Van Dorn Street",
-    "Van Ness UDC", "Vienna", "Virginia Square GMU", "Waterfront",
-    "West Falls Church", "West Hyattsville", "Wheaton", "White Flint",
-    "Wiehle Reston East", "Woodley Park"]; 
+const wordList = ["Pikachu","Charmander","Bulbasaur","Mew",
+"Totodile","Torchi","Treeko","Raikou","Arceus","Mewtwo",
+"Pidgey","Torterra","Dugtrio","Psyduck","Wingull",
+"Vulpix","Jynx","Pikachu","Snorlax","Ludicolo","Growlithe",
+"Cyndaquil","Lucario","Onix","Geodude","Chansey",];
 
-    // Select a word from the given list
-    function randomWord(wordList) {
-        var index = Math.floor(Math.random() * wordList.length);
-        return wordList[index];
-    }
+function randomWord(wordList) {
+    var index = Math.floor(Math.random() * wordList.length);
+    return wordList[index];
+}
 
 const questions = [
-    {
-        name: 'letterGuessed',
-        message: 'Guess a letter',
-        validate: function (value) {
-            var valid = (value.length === 1) && ('abcdefghijklmnopqrstuvwxyz'.indexOf(value.charAt(0).toLowerCase()) !== -1); // fix letter logic later
-            return valid || 'Please enter a single letter';
-        },
-        when: function () {
-            return (!target.allGuessed() && guessesLeft > 0);
-        }
+{
+    name: 'letterGuessed',
+    message: 'Guess a letter',
+    validate: function (value) {
+        var valid = (value.length === 1) && ('abcdefghijklmnopqrstuvwxyz'.indexOf(value.charAt(0).toLowerCase()) !== -1); // fix letter logic later
+        return valid || 'Please enter a single letter';
     },
-    {
-        type: 'confirm',
-        name: 'playAgain',
-        message: 'Want to play again?',
-        // default: true,
-        when: function () {
-            return (target.allGuessed() || guessesLeft <= 0);
-        }
+    when: function () {
+        return (!target.allGuessed() && guessesLeft > 0);
     }
+},
+{
+    type: 'confirm',
+    name: 'playAgain',
+    message: 'Want to play again?',
+    // default: true,
+    when: function () {
+        return (target.allGuessed() || guessesLeft <= 0);
+    }
+}
 ];
 
 function resetGame() {
-    targetWord = randomWord(wordList);
-    // console.log(targetWord);
-    target = new Word(targetWord);
-    target.makeGuess(' ');
-    guesses = [];
-    guessesLeft = 9;
+targetWord = randomWord(wordList);
+// console.log(targetWord);
+target = new Word(targetWord);
+target.makeGuess(' ');
+guesses = [];
+guessesLeft = 9;
 }
 
 function ask() {
-    // console.log('target.allGuessed():', target.allGuessed());
-    if (!target.allGuessed() && guessesLeft > 0) {
-        console.log(target + '');
+// console.log('target.allGuessed():', target.allGuessed());
+if (!target.allGuessed() && guessesLeft > 0) {
+    console.log(target + '');
+}
+
+inquirer.prompt(questions).then(answers => {
+    // console.log('answers.playAgain ' + answers.playAgain);
+    if ('playAgain' in answers && !answers.playAgain) {
+        console.log('thanks for playing');
+        process.exit();
     }
-    
-    inquirer.prompt(questions).then(answers => {
-        // console.log('answers.playAgain ' + answers.playAgain);
-        if ('playAgain' in answers && !answers.playAgain) {
-            console.log('thanks for playing');
-            process.exit();
-        }
-        if (answers.playAgain) {
-            resetGame();
-        }
+    if (answers.playAgain) {
+        resetGame();
+    }
 
-        if (answers.hasOwnProperty('letterGuessed')) {
-            var currentGuess = answers.letterGuessed.toLowerCase();
+    if (answers.hasOwnProperty('letterGuessed')) {
+        var currentGuess = answers.letterGuessed.toLowerCase();
+        
+        if (guesses.indexOf(currentGuess) === -1) {
+            guesses.push(currentGuess);
+            target.makeGuess(currentGuess);
+            if (targetWord.toLowerCase().indexOf(currentGuess.toLowerCase()) === -1) {
+                guessesLeft--;
+            }
+        } else {
+            console.log('you already guessed', currentGuess);
             
-            if (guesses.indexOf(currentGuess) === -1) {
-                guesses.push(currentGuess);
-                target.makeGuess(currentGuess);
-                if (targetWord.toLowerCase().indexOf(currentGuess.toLowerCase()) === -1) {
-                    guessesLeft--;
-                }
-            } else {
-                console.log('you already guessed', currentGuess);
-                
-            }
+        }
+    }
+
+    if (!target.allGuessed()) {
+        if (guessesLeft < 1) {
+            console.log('no more guesses');
+            console.log(targetWord, 'was correct.');
+
+        } else {
+            console.log('guesses so far:', guesses.join(' '));
+            console.log('guesses remaining:', guessesLeft);
         }
 
-        if (!target.allGuessed()) {
-            if (guessesLeft < 1) {
-                console.log('no more guesses');
-                console.log(targetWord, 'was correct.');
+    } else {
+        console.log(targetWord, 'is correct!');
+        // console.log(answers.playAgain);
+    }
 
-            } else {
-                console.log('guesses so far:', guesses.join(' '));
-                console.log('guesses remaining:', guessesLeft);
-            }
-
-        } else { 
-            console.log(targetWord, 'is correct!');
-            // console.log(answers.playAgain);
-        }
-
-        ask();
-    }); // end inquirer.then
+    ask();
+}); // end inquirer.then
 }
 resetGame();
 ask();
